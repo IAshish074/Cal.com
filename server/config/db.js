401afe6ffa9a73570c46924d7dbe61a24a71dce2
Ashish Kumar Mishra <amishrahsm@gmail.com>
2026-04-14 23:05:07 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,12 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.DB_URL);
+const pool = mysql.createPool({
+  uri: process.env.DB_URL,   // ✅ correct way
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 console.log("MySQL connected successfully 🚀");
 
